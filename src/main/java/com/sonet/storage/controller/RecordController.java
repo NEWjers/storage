299f430518ac9afc3a47073d554dc97378fba876
Serhiy Pityk <serjsonet@gmail.com>
2022-12-06 11:02:01 +0200

    Report for record

--- src/main/java/com/sonet/storage/controller/RecordController.java
+++ src/main/java/com/sonet/storage/controller/RecordController.java
@@ -2,7 +2,13 @@ package com.sonet.storage.controller;
 
 import com.sonet.storage.dto.response.RecordResponse;
 import com.sonet.storage.service.RecordService;
+import com.sonet.storage.service.report.RecordReportService;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +21,9 @@ public class RecordController {
 
     @Autowired
     private RecordService recordService;
+
+    @Autowired
+    private RecordReportService recordReportService;
 
     @GetMapping("page")
     @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
@@ -47,5 +56,30 @@ public class RecordController {
                                     @RequestParam(name = "count") String count) {
 
         return recordService.getAllRecordsSize(id, code, itemSize, pack, price, description, producer, count).size();
+    }
+
+    @GetMapping("report")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public ResponseEntity<byte[]> getRecordsReport(@RequestParam(name = "sort") String sort,
+                                                   @RequestParam(name = "way") String way,
+                                                   @RequestParam(name = "id") String id,
+                                                   @RequestParam(name = "code") String code,
+                                                   @RequestParam(name = "itemSize") String itemSize,
+                                                   @RequestParam(name = "pack") String pack,
+                                                   @RequestParam(name = "price") String price,
+                                                   @RequestParam(name = "description") String description,
+                                                   @RequestParam(name = "producer") String producer,
+                                                   @RequestParam(name = "count") String count) throws JRException {
+
+        byte[] content = recordReportService.generateReport(sort, way, id, code, itemSize, pack, price, description, producer, count);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PDF);
+
+        String filename = "records.pdf";
+        headers.setContentDispositionFormData("filename", filename);
+        headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+
+        return new ResponseEntity<>(content, headers, HttpStatus.OK);
     }
 }
